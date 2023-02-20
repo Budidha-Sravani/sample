50db6d469af1f1b685f4ea7a3e28fb5f56e186db
Budidha-Sravani <116790560+Budidha-Sravani@users.noreply.github.com>
2023-02-20 17:15:10 +0530

    updated

---
+++ App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import GiftPolicy from './components/GiftPolicy';
+//import  AppointmentLetter from './components/AppointmentLetter';
+//import JoiningLetter from './components/JoiningLetter';
+//import CyberSecurityPolicies from './components/CyberSecurityPolicies'
+//import BCP from './components/BCP';
+//import CorporateGovernance from './components/CorporateGovernance';
+//import SCD from './components/SCD';
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <GiftPolicy/>
+
+
+    </div>
+  );
+}
+export default App;
---
+++ GiftPolicy.js
@@ -0,0 +1,92 @@
+import React from 'react';
+function GiftPolicy() {
+    const imgeurl="https://cdn.americanfinancing.net/images/gallery/learning-center/tlc_featured/gift-rules_2x.jpg?1519248210"
+  return (
+    <div className='Gift'>
+    <div className='image' style={{backgroundImage:`url(${imgeurl})` ,backgroundRepeat:"no-repeat",backgroundSize:"1600px",height:"570px",width:"1510px"}}>
+        <div className='GiftContainer'>
+        <span style={{ fontWeight: 'bold', color: 'white',textAlign: 'center' }}>
+            <h1 className='title'>GIFTING POLICY</h1>
+            </span>
+        </div>
+    </div>
+    <div style={{ textAlign: 'left', padding: '80px' }}>
+    <div>
+      <span class="GiftStyles">
+      <h4>Introduction</h4>
+      </span>
+      <p></p>
+      <p>The YB Code of Conduct is a comprehensive document which serves as the ethical roadmap to YB employees. Our collective adherence to these guidelines represents our promise to ourselves and to the many stakeholders of brands . Each YB employee is responsible to ensure that his or her behaviour and actions, both individual and collective, stay aligned to these values. Business gifts and hospitality are occasionally used in the course of business activity as a means to build goodwill and strengthen working relationships among business associates. However, gifts or hospitality (including entertainment or travel) may create conflict of interest or illicit payment. YB has adopted this policy to help its employees take the right decisions when they are offered gifts or hospitality while conducting business or official transactions on behalf of YB</p>
+      <p></p>
+      <span class="GiftStyles" >
+      <h4>Definitions</h4>
+      </span>
+      <p></p>
+      <li>
+      The term “gift” would include any gratuitous non- monetary benefit which can be used or consumed
+      </li>
+      <li>
+      The term “hospitality” would include any form of travel, hotel, food, drinks, entertainment or any events (participating or watching) such as sporting events, theatrical events, awards or ceremonies.
+      </li>
+      <li>
+      The term “business associate” would include suppliers, customers, vendors, dealers, distributors, franchisees, lessors, lessees or such other persons with whom YB has any business or transactional dealings
+      </li>
+      <p></p>
+      <span class="GiftStyles" >
+      <h4>Policy</h4>
+      </span >
+      <p></p>
+     <p class="GiftStyles"> 1. Accepting Gifts</p>
+      <p></p>
+      <p>Employees must be very careful when it comes to accepting gifts. As a general rule, employees should discourage business associates from giving gifts. However, they may accept a gift from a business associates provided such a gift:</p>
+      <li>
+      Is exchanged during festivals of other ceremonial occasions, commensurate with the culture and occasion and is limited to flowers, sweets or any other eatables.
+      </li>
+      <li>Is exchanged during New Year, articles of office use like stationaries, desk accessories with logo of the donor Company</li>
+      <li>Received at an event of ceremonial nature (e.g. a customer outing or in honour of a business transaction) and is impractical and offensive to refuse.</li>
+     <li>In all above cases value of gifts should never exceed INR 3000</li>
+     <p>Any gift other than mentioned above needs to be returned back to the donor with a timely and appropriate note explaining to the gift donor the rationale for returning the gift.</p>
+     <p>All gifts with value above INR 3000 are to be reported to the recipient’s Departmental Head and the Ethics Counselor. These gifts should be deposited in the office of the Ethics Counselor; records of such gifts should be maintained in the gift register</p>
+     <p>The following gifts are never appropriate and should never be accepted:</p>
+     <li>Monetary benefit of any value under any circumstance;</li>
+     <li>Gift of cash or gold or other precious metals, gems or stones;</li>
+     <li>Gifts being given outside the workplace or venue of business event;</li>
+     <li>Gifts given in the form of service or other non-cash benefit (e.g. a promise of employment).</li>
+     <p></p>
+     <p class="GiftStyles">2. Accepting Hospitality </p>
+     <p></p>
+     <p>Business hospitality (e.g. meals, tickets to theatre or sporting event) may be provided to strengthen working relationships among business associates. However, hospitality applies to situations in which the host is present. Tickets to sporting or cultural events provided to YB employees and not attended by host are essentially “gifts” and not “hospitality” and hence should be dealt in accordance with the gift guidelines specified above</p>
+     <p>The following is never appropriate and such hospitality should never be accepted.</p>
+     <li>Hospitality that can be viewed as creating any affiliation of YB with any particular political party;</li>
+     <li>Interaction in locations/establishments generally not recognised as appropriate for the conduct of business;</li>
+     <li>Interaction in adult entertainment clubs or at adult/inappropriate events are expressly prohibited</li>
+     <p>Employees should consult the Departmental Head or Ethics Counselor when in doubt as to whether an event, location or expenditure is appropriate or not.</p>
+     <p></p>
+     <p class="GiftStyles">3. Travel (Other than normal company business travel)</p>
+     <p></p>
+     <p>It is acknowledged that hosting business associates at our premises to promote our business interests and reciprocal visits to our business associates are an important aspect of our business relationships. However, in addition to the guidance on appropriateness of hospitality received, one should also consider the guidelines below in the case of promotional trips;</p>
+     <li>Daily allowance, cash allowances or cash payments of any nature must not be accepted from business associate for such trips;</li>
+    <li>No overnight accommodation is to be accepted from a business associate;</li>
+      <li>In circumstances where participation in a vendor-supported event is deemed necessary for a business and bulk booking arrangements are made by the vendor for travel and/or accommodation, the pro-rata costs should either be reimbursed to the vendor or preferably paid directly to the provider of travel and/or accommodation</li>
+      <p>Ideally, family members or persons not directly related to the relevant business project should not join these trips/events. However, if they do attend, their expenses should be fully paid for by the concerned employee and not paid or reimbursed by TBSL or the business associate. Moreover, such instances of travel should be informed to the Departmental Head and Ethics Counselor prior to travel to ensure transparency through disclosure</p>
+      <p>Any exception to the above should have a specific approval of the Managing Director/ Vice President (Building Solutions) and should be declared in the Gift Register</p>
+      <p></p>
+      <span class="GiftStyles">
+      <h4>Deployment Effectiveness</h4>
+      </span>
+      <p></p>
+<p>Implementation of this policy would be measured through compliance and disclosure in the Gift Register</p>
+<p>All gifts with value above INR 3000, donations and entertainment received by a YB employee should be disclosed in the Gift Register along with the context/business purpose of the gift, reasons for accepting, whether the gift been deposited with Ethics Counselor.</p>
+<p>Employees should consult the Ethics Counselor when in doubt as to whether a gift, hospitality or travel is appropriate or not.</p>
+<p>This policy applies to all employees of YB and its subsidiary companies. If stricter norms are prescribed under any applicable law with respect to gifts and hospitality, then, employees shall comply with such stricter norms.</p>
+<p></p>
+<b>Karan.T</b>
+<p></p>
+<b>Managing Director</b>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default GiftPolicy;
